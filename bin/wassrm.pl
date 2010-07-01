6bbe1e90ccf234d18969648cf88d82f38022394e
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2010-07-01 23:03:52 +0900

    add commandline tool
    Signed-off-by: SUZUKI Masashi <m15.suzuki.masashi@gmail.com>

---
+++ bin/wassrm.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+
+use Encode;
+use Getopt::Long;
+use Net::WassrMinus;
+
+sub main {
+    # オプション取得
+    my ($file, $user, $password);
+
+    GetOptions(
+        'file=s'     => \$file,
+        'userd=s'    => \$user,
+        'password=s' => \$password,
+    );
+
+    my $wassr;
+    if (defined $file) {
+        die 'config is not exist' if (not -f $file);
+        $wassr = Net::WassrMinus::new_with_config($file);
+    }
+    elsif (defined $user and defined $password) {
+        $wassr = Net::WassrMinus->new({
+            user     => $user,
+            password => $password,
+        });
+    }
+    else {
+        $wassr = Net::WassrMinus::new_with_config($file);
+    }
+
+    # コマンド取得
+    while (my $command = get_command()) {
+        if ($command ne '' and $command =~ /^q|(quit)$/){
+            exit 1;
+        }
+        elsif (not defined $command or $command =~ /^(help)|(--help)$/ or
+            not $command =~ /^(update)|(user)|(friends)|(replies)|(public)|(show)$/ ) {
+            help();
+            next;
+        }
+        $command = $command . '_timeline' if $command =~ /^(frieds)|(public)|(user)$/;
+
+        my $comments = $wassr->$command;
+        for my $comment (@{$comments}) {
+            print $comment->{user_login_id}, "\n";
+            print encode('utf8', $comment->{text}), "\n";
+        }
+        $command = undef;
+    }
+
+}
+
+sub help {
+    #TODO: write help message
+    print "help messagge\n";
+}
+
+sub get_command {
+    print 'command? : ';
+    chomp (my $command = <STDIN>);
+    return $command;
+}
+
+
+main if not caller(0);
